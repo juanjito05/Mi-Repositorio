2af5cdccdd5172c3a8ee6bc72f2066e4e2750d1c
Juanjo <m@juanjoseortizj.sanjosemlg>
2025-09-25 12:22:36 +0200

    Primer proyecto java

---
+++ JAVA/InicionCurso/src/arrayList/contraseña.java
@@ -0,0 +1,33 @@
+package arrayList;
+
+import java.util.Scanner;
+
+public class contraseña {
+
+	public static void main(String[] args) {
+		String palabra;
+		char[] letras;
+		int totalLetras = 0;
+//		Introducir por teclado
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Introduzca la palabra");
+		palabra = sc.nextLine();
+		
+//		Agregar el texto introducido al array de palabras
+		
+		letras = new char[palabra.length()];
+		System.out.println(palabra);
+		
+//		bucle paara leer el array de letras gyardadas y convertir en caracteres 
+		for (int i = 0; i < letras.length; i++) {
+			letras[i] = palabra.charAt(i);
+		}
+		
+		for (int i = 0; i < letras.length; i++) {
+			System.out.println(letras[i]);
+			
+		}
+		
+	}
+}
