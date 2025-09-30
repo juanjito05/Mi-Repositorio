02740666be3beaa73919e59d17f81a57a3f2d010
Juanjo <m@juanjoseortizj.sanjosemlg>
2025-09-30 12:19:23 +0200

    Se agregó nueva clase

---
+++ JAVA/InicionCurso/src/arrayList/contraseña1.java
@@ -0,0 +1,22 @@
+package arrayList;
+
+import java.util.Scanner;
+
+public class contraseña1 {
+
+	public static void main(String[] args) {
+
+//		cpntraseña a introducir
+		String palabra;
+//		Contraseña preestablecida
+		String contraseña = "hola";
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Introduzca la palabra");
+		palabra = sc.nextLine();
+
+		while (palabra != contraseña) {
+
+		}
+	}
+}
